7fb67502dabcc6c859f9eead1625f22f59635edd
neha45556 <neha45556@gmail.com>
2019-11-01 19:59:41 -0700

    Update main.c

--- lab09-pwm/source/main.c
+++ lab09-pwm/source/main.c
@@ -45,7 +45,7 @@ void PWM_off(){
 double sound[8] = {261.63,293.66,329.63,349.23,392.00,440.00,493.88,523.25}
 unsigned char  i ;
 
-enum states(init, button1,button2,button3) states;
+enum states{init, button1,button2,button3} states;
     
     void sound(){
         switch(state){
